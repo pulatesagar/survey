2b1d375cb7edb5b0ea208d92aeffcd4b173c6529
pulatesagar <sagarpulate@email.com>
2020-11-27 14:29:17 +0530

    few changes

--- src/tests/createsurvey.test.ts
+++ src/tests/createsurvey.test.ts
@@ -23,7 +23,7 @@ let s_survey_accessability_option_value = 'UserGroup';
 
 
 beforeAll(async () => {
-  browser = await chromium.launch({ headless: false , devtools:true,slowMo:500});
+  browser = await chromium.launch({ headless: false, slowMo:10});
   context = await browser.newContext();
   page = await context.newPage();
   await page.goto(create_survey_page_url);
@@ -71,13 +71,15 @@ describe('Create Survey Scenarios', () => {
     }
 
     finalSurveyDate = yyyy + "-" + mm + "-" + day;
-  
+    // Click on create survey button and navigate to survey form
     await page.click(s_create_survey_button);
+    // Select Active status
     await page.check(s_survey_status);
-    await page.fill(s_survey_name, s_survey_name_value + " " + a);
+    // Fill survey
+    await page.type(s_survey_name, s_survey_name_value + " " + a);
     await page.fill(s_survey_expiry, finalSurveyDate);
-    await page.fill(s_survey_url, s_survey_url_value);
-    await page.fill(s_survey_from_email_address, s_survey_from_email_address_value);
+    await page.type(s_survey_url, s_survey_url_value);
+    await page.type(s_survey_from_email_address, s_survey_from_email_address_value);
     await page.check(s_survey_trigger_caseclosure);
     await page.check(s_survey_trigger_activityclosure);
     if(a < 4)
@@ -94,9 +96,9 @@ describe('Create Survey Scenarios', () => {
     // await page.waitForSelector('text="eGain Survey 1"');
     // await page.waitForSelector('text="Active"');
     const createdsurvey = await page.textContent('//html/body/div/div/div[2]/table/tbody/tr/td[1]');
-    console.log("Created survey is" + " " + createdsurvey);
+    console.log("Created survey is" + " " +createdsurvey);
     const surveystatus = await page.textContent('//html/body/div/div/div[2]/table/tbody/tr/td[5]');
-    console.log("Created survey status is " + " " + surveystatus);
+    console.log("Created survey status is" + " " +surveystatus);
     expect(createdsurvey).toBe(s_survey_name_value + " " + a);
     expect(surveystatus).toBe(s_survey_status_value);
 
