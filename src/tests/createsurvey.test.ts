867772f98c9794bf0de1dfe4d9d1db1e8cc1c4fb
pulatesagar <sagarpulate@email.com>
2020-12-03 18:05:02 +0530

    dropdown code added

--- src/tests/createsurvey.test.ts
+++ src/tests/createsurvey.test.ts
@@ -21,11 +21,11 @@ var surveyAccessabilityOptionValue = 'UserGroup';
 
 // browser setup
 beforeAll(async () => {
-  browser = await chromium.launch({ headless: false, slowMo:10});
+  browser = await chromium.launch({ headless: false, slowMo: 10 });
   context = await browser.newContext();
   page = await context.newPage();
   await page.goto(createSurveyPageUrl);
-  
+
 });
 // object dispose
 afterAll(async () => {
@@ -36,89 +36,83 @@ afterAll(async () => {
 
 describe('Create Survey Scenarios', () => {
 
-  test.each([[1], [2], [3], [4], [5]])
 
-    ('Create Survey %i', async (a) => {
-    let month, day, finalSurveyExpiryDate;
-    var currentDate = new Date();
-    var surveyDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() + a);
-    var mm = surveyDate.getMonth() + 1; // added 1 to get correct current month
-    var dd = surveyDate.getDate(); // increment day's by 1
-    var yyyy = surveyDate.getFullYear(); 
-    // checking date formart is less than two digit, then appending 0 before the day 
-    if (dd < 10) {
-      day = '0' + dd.toString();
+
+  test("Create Survey", async () => {
+    var i;
+    for (i = 1; i <= 10; i++) {
+      var index = i % 4;
+      let month, day, finalSurveyExpiryDate;
+      var currentDate = new Date();
+      var surveyDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() + i);
+      var mm = surveyDate.getMonth() + 1; // added 1 to get correct current month
+      var dd = surveyDate.getDate(); // increment day's by 1
+      var yyyy = surveyDate.getFullYear();
+      // checking date formart is less than two digit, then appending 0 before the day 
+      if (dd < 10) {
+        day = '0' + dd.toString();
+      }
+      else {
+        day = dd.toString();
+      }
+      // checking month formart is less than two digit, then appending 0 before the month 
+      if (mm < 10) {
+        month = '0' + mm.toString();
+      }
+      else {
+        month = mm.toString();
+      }
+
+      finalSurveyExpiryDate = yyyy + "-" + mm + "-" + day;
+      // Click on create survey button and navigate to survey form
+      await page.click(createSurveyButton);
+      // Select Active status
+      await page.check(surveyStatus);
+      // Enter Survey Name
+      await page.type(surveyName, surveyNameValue + " " + i);
+      // Fill Survey Expiry Date
+      await page.fill(surveyExpiry, finalSurveyExpiryDate);
+      // Enter Survey URL
+      await page.type(surveyUrl, surveyUrlValue);
+      // Enter Survey From Email Address
+      await page.type(surveyFromEmailAddress, surveyFromEmailAddressValue);
+      // Select Survey Trigger
+      await page.check(surveyTriggerCaseClosure);
+      await page.check(surveyTriggerActivityClosure);
+      // Select different options from dropdown
+
+      await page.selectOption(surveyAccessabilityOption, surveyAccessabilityOptionValue + (index == 0 ? 4 : index));
+
+      // Submit the form
+      await page.click('text="Submit"');
+
+      // page scraping for data verification for created survey name
+      const createdSurvey = await page.textContent('//html/body/div/div/div[2]/table/tbody/tr/td[1]');
+
+      // demo purpose, print created survey name
+      console.log("Created survey is:" + " " + createdSurvey);
+
+      // page scraping for data verification for selected survey status
+      const surveyStatusCheck = await page.textContent('//html/body/div/div/div[2]/table/tbody/tr/td[5]');
+
+      // demo purpose, print created survey status
+      console.log("Created survey status is:" + " " + surveyStatusCheck);
+
+      // verify the created survey appears on page and are same value
+      expect(createdSurvey).toBe(surveyNameValue + " " + i);
+
+      // verify the selected survey status appears on page and are same value
+      expect(surveyStatusCheck).toBe(surveyStatusValue);
+
+      // take a screenshot of created survey
+      const data = await page.screenshot({ path: './images/Survey.jpg' });
+
+      // attach screenshot to report
+      await addAttach(data, "Survey" + " " + i);
+
+      // demo purpose
+      await page.waitForTimeout(1000)
     }
-    else
-    {
-      day=dd.toString();
-    }
-    // checking month formart is less than two digit, then appending 0 before the month 
-    if (mm < 10) {
-      month = '0' + mm.toString();
-    }
-    else
-    {
-      month=mm.toString();
-    }
-
-    finalSurveyExpiryDate = yyyy + "-" + mm + "-" + day;
-    // Click on create survey button and navigate to survey form
-    await page.click(createSurveyButton);
-    // Select Active status
-    await page.check(surveyStatus);
-    // Enter Survey Name
-    await page.type(surveyName, surveyNameValue + " " + a);
-    // Fill Survey Expiry Date
-    await page.fill(surveyExpiry, finalSurveyExpiryDate);
-    // Enter Survey URL
-    await page.type(surveyUrl, surveyUrlValue);
-    // Enter Survey From Email Address
-    await page.type(surveyFromEmailAddress, surveyFromEmailAddressValue);
-    // Select Survey Trigger
-    await page.check(surveyTriggerCaseClosure);
-    await page.check(surveyTriggerActivityClosure);
-    // Select different options from dropdown
-    if(a < 4)
-    {
-    await page.selectOption(surveyAccessabilityOption, surveyAccessabilityOptionValue+a);
-    }
-    else
-    {
-      surveyAccessabilityOptionValue = 'UserGroup1';
-      await page.selectOption(surveyAccessabilityOption, surveyAccessabilityOptionValue);
-    }
-
-    // Submit the form
-    await page.click('text="Submit"');
-
-    // page scraping for data verification for created survey name
-    const createdSurvey = await page.textContent('//html/body/div/div/div[2]/table/tbody/tr/td[1]');
-
-    // demo purpose, print created survey name
-    console.log("Created survey is:" + " " +createdSurvey); 
-
-    // page scraping for data verification for selected survey status
-    const surveyStatusCheck = await page.textContent('//html/body/div/div/div[2]/table/tbody/tr/td[5]');
-
-    // demo purpose, print created survey status
-    console.log("Created survey status is:" + " " +surveyStatusCheck);  
-
-    // verify the created survey appears on page and are same value
-    expect(createdSurvey).toBe(surveyNameValue + " " + a);
-
-    // verify the selected survey status appears on page and are same value
-    expect(surveyStatusCheck).toBe(surveyStatusValue);
-
-    // take a screenshot of created survey
-    const data = await page.screenshot({ path: './images/Survey.jpg'});
-
-    // attach screenshot to report
-    await addAttach(data, "Survey"+ " " +a);
-
-    // demo purpose
-    await page.waitForTimeout(1000) 
-
   });
 
 });
